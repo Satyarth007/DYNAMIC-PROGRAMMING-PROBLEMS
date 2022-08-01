1ffd05e45930ad9d0700bdadde7f28e1e68ce96c
SATYARTH <satyarthtiwari123@gmail.com>
2022-08-01 16:45:52 +0530

    Create climbingStairs.java

---
+++ climbingStairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+       if(n <= 2) return n; //if 1 or 2 
+        
+        int[] ans = new int[n + 1];
+        ans[1] = 1;
+        ans[2] = 2;
+        
+        for(int i = 3; i <= n; i++) 
+            ans[i] = ans[i - 1] + ans[i - 2];      // it will end up as fibonnaci series
+                                                   
+        return ans[n];
+        
+    
+    
+    
+    }
+}
